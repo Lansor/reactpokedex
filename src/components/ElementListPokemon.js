2e38cd954684dbd627cb60c0d518f819301334f2
NathanBarbier <81628062+NathanBarbier@users.noreply.github.com>
2023-05-23 17:12:12 +0200

    Petit fix pour rendre la ligne cliquable (#4)

--- src/components/ElementListPokemon.js
+++ src/components/ElementListPokemon.js
@@ -1,15 +1,22 @@
 import Types from './Types'
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const ElementListPokemon = ({ id, nom, types }) => {
     return (
         <>
-            <div className='row w-75 m-auto'>
+            <Link
+                to={`/pokemon/${id}`}
+                className='row w-75 m-auto'
+                style={{ textDecoration: ' none', color: 'white' }}
+            >
                 <div className='col-2'>
-                    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} alt={nom} />
+                    <img
+                        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
+                        alt={nom}
+                    />
                 </div>
                 <div className='col-2 my-auto'>{id}</div>
-                <div className='col-5 my-auto'><Link to={`/pokemon/${id}`}>{nom}</Link></div>
+                <div className='col-5 my-auto'>{nom}</div>
                 <div className='col-3 my-auto'>
                     <div className='row'>
                         {types.map((data, id) => {
@@ -17,7 +24,7 @@ const ElementListPokemon = ({ id, nom, types }) => {
                         })}
                     </div>
                 </div>
-            </div>
+            </Link>
         </>
     )
 }
